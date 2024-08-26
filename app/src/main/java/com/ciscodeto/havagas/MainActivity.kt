42f51c6e2b71c041a21d466d87009cce156c0f47
ciscodeto <netofcisco@gmail.com>
2024-08-26 10:28:47 -0300

    fix: variables and ids for visibility gone items

--- app/src/main/java/com/ciscodeto/havagas/MainActivity.kt
+++ app/src/main/java/com/ciscodeto/havagas/MainActivity.kt
@@ -10,25 +10,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ciscodeto.havagas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val nameET: EditText by lazy { findViewById(R.id.nameET) }
-    private val emailET: EditText by lazy { findViewById(R.id.emailET) }
-    private val joinEmailListCB: CheckBox by lazy { findViewById(R.id.joinEmailCB) }
-    private val phoneET: EditText by lazy { findViewById(R.id.phoneET) }
-    private val businessRB: RadioButton by lazy { findViewById(R.id.businessRB) }
-    private val homeRB: RadioButton by lazy { findViewById(R.id.homeRB) }
-    private val cellPhoneET: EditText by lazy { findViewById(R.id.cellPhoneET) }
-    private val birthDateET: EditText by lazy { findViewById(R.id.birthDateET) }
-    private val joinEmailCB: CheckBox by lazy { findViewById(R.id.joinEmailCB) }
-    private val addCellPhoneCB: CheckBox by lazy { findViewById(R.id.addCellPhoneCB) }
-    private val sexSpn: Spinner by lazy { findViewById(R.id.sexSpn) }
-    private val clearBtn: Button by lazy { findViewById(R.id.clearBtn) }
-    private val saveBtn: Button by lazy { findViewById(R.id.saveBtn) }
+    private val amb by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        setContentView(amb.root)
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
